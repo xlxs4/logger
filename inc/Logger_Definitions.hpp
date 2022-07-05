3bd4c78293b8ba69f6ea8d33a2d2014d4fd26619
xlxs4 <orousoultzoglou@gmail.com>
2022-07-05 12:59:10 +0300

    Initial commit

---
+++ inc/Logger_Definitions.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @defgroup LoggerDefinitions Defined Constants
+ *
+ * This file contains constant definitions that are used in the logger configuration.
+ *
+ * @{
+ */
+
+/**
+ * @file
+ * This file contains constant definitions that are used in the logger configuration.
+ * @see LoggerDefinitions
+ */
+
+/**
+ * @brief The maximum size of a log message
+ */
+#define LOGGER_MAX_MESSAGE_SIZE 512
